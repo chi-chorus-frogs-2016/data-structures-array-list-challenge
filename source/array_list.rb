d44e6c79d32dce58979bb5ef5c1b1b0791963d5a
loganbresnahan <loganbbres@gmail.com>
2016-03-08 18:43:08 -0600

    Stuff from the bus

--- source/array_list.rb
+++ source/array_list.rb
@@ -1,3 +1,31 @@
+require_relative 'fixed_array'
+
 class ArrayList
+  attr_reader :array
+
+  def initialize(fixed_array)
+    @array = fixed_array
+  end
+
+  def add(element)
+    new_array = FixedArray.new(@array.size * 2)
+    new_array.set(@array.size, element)
+    third_array = FixedArray.new(1, new_array)
+    # new_array.slice!(@array.size + 2, -1)
+    @array = new_array
+  end
+
+  def get(index)
+  end
+
+  def set(index, element)
+  end
+
+  def size
+  end
 
 end
+
+array = ArrayList.new(FixedArray.new)
+array.add("yes")
+p array
--- source/array_list_spec.rb
+++ source/array_list_spec.rb
@@ -2,4 +2,39 @@ require_relative 'array_list'
 
 describe ArrayList do
 
+  describe '#initialize' do
+    let(:array) { ArrayList.new(FixedArray.new) }
+    it 'creates a new ArrayList' do
+      expect(array.array).to be_a(FixedArray)
+    end
+  end
+
+  describe '#add' do
+    let(:array) { ArrayList.new(FixedArray.new) }
+    it 'will add an element to the fixed array' do
+      expect(array.add('yes')).to eq('yes')
+    end
+  end
+
+  describe '#get' do
+    let(:array) { ArrayList.new(FixedArray.new) }
+    it 'will retrieve an element from a given index' do
+      expect(array.get(0)).to eq(nil)
+    end
+  end
+
+  describe '#set' do
+    let(:array) { ArrayList.new(FixedArray.new) }
+    it 'will set and element at a given index' do
+      expect(array.set(0, 'no')).to eq('no')
+    end
+  end
+
+  describe '#size' do
+    let(:array) { ArrayList.new(FixedArray.new) }
+    it 'will show the length of the array' do
+      expect(array.size).to eq(4)
+    end
+  end
+
 end
